ed3f19a4a769f63c354fa815f626941c804a9e80
ImranulSiddique1987 <imranul.siddique@sql-g.com>
2026-07-11 00:22:33 +0530

    chore(repo): synchronize shared eslint flat configuration

--- packages/eslint/base.mjs
+++ packages/eslint/base.mjs
@@ -1,3 +1,33 @@
 import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default [js.configs.recommended];
+export default [
+  {
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/.turbo/**",
+      "**/coverage/**",
+    ],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    rules: {},
+  },
+];
